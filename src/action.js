890af6b9c49dc762b5bfb88b7392d8e3e429a71e
Sibi Sivasubramanian <sibiwitty@gmail.com>
2021-10-19 21:03:35 -0700

    add PR message

--- src/action.js
+++ src/action.js
@@ -1,5 +1,23 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
 async function run() {
-  console.log('Hello, world!');
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  // Getting a new instance of Octokit with our GitHub token
+  // Destructuring the context of our active job
+  // Finding the pull request from our active context
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
+  });
 }
 
 run();
